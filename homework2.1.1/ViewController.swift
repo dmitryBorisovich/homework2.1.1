36d4b273ca6b90787df244d1653897672c182c09
dima <lyubomirovdb@gmail.com>
2024-02-03 05:16:40 -0800

    Initial Commit

---
+++ homework2.1.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homework2.1.1
+//
+//  Created by dmitry on 03.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
